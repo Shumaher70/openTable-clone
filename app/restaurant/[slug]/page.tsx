472312d0fa9e7871ba47ffdabfe294017c56dffc
oleg <olegvraby@gmail.com>
2023-10-24 19:00:38 +0200

    get data for RestaurantDetails

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,6 +1,5 @@
-import NavBar from '../../components/NavBar';
+import { PrismaClient } from '@prisma/client';
 import Description from './components/Description';
-import Header from './components/Header';
 import Images from './components/Images';
 import Rating from './components/Rating';
 import ReservationCart from './components/ReservationCart';
@@ -9,7 +8,42 @@ import Reviews from './components/Reviews';
 import Title from './components/Title';
 import Head from './head';
 
-const RestaurantDetails = () => {
+const prisma = new PrismaClient();
+
+interface RestaurantDetailsType {
+   id: number;
+   name: string;
+   images: string[];
+   description: string;
+   slug: string;
+}
+
+const fetchRestaurantBySlug = async (
+   slug: string
+): Promise<RestaurantDetailsType> => {
+   const restaurant = await prisma.restaurant.findUnique({
+      where: {
+         slug,
+      },
+      select: {
+         id: true,
+         name: true,
+         images: true,
+         description: true,
+         slug: true,
+      },
+   });
+
+   if (!restaurant) {
+      throw new Error();
+   }
+
+   return restaurant;
+};
+
+const RestaurantDetails = async ({ params }: { params: { slug: string } }) => {
+   const restaurant = await fetchRestaurantBySlug(params.slug);
+
    return (
       <>
          <Head />
