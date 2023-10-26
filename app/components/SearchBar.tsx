2d3acca6e6bcf195b25eabe9f1d0df54fd26140b
oleg <olegvraby@gmail.com>
2023-10-26 18:18:57 +0200

    add dynamic query city for SearchBar

--- app/components/SearchBar.tsx
+++ app/components/SearchBar.tsx
@@ -18,8 +18,9 @@ const SearchBar = () => {
          <button
             className="rounded bg-red-600 px-9 py-2 text-white"
             onClick={() => {
-               if (location === 'banana') return;
-               router.push('/search');
+               if (location === '') return;
+               router.push(`/search?city=${location}`);
+               setLocation('');
             }}
          >
             {` Let's go`}
