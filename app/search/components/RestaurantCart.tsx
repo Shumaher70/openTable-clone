c7089949f73dfe7a0fa183ed0b93fe87f7dd7d40
oleg <olegvraby@gmail.com>
2023-10-26 18:22:00 +0200

    add dynamic data for RestaurantCart

--- app/search/components/RestaurantCart.tsx
+++ app/search/components/RestaurantCart.tsx
@@ -1,28 +1,36 @@
 import Link from 'next/link';
+import { RestaurantCardType } from '../../page';
+import Image from 'next/image';
+import Price from '../../components/Price';
 
-const RestaurantCart = () => {
+const RestaurantCart = ({ restaurant }: { restaurant: RestaurantCardType }) => {
    return (
-      <div className="border-b flex pb-5">
-         <img
-            src="https://images.otstatic.com/prod1/49153814/2/medium.jpg"
-            alt=""
-            className="w-44 rounded"
+      <div className="border-b flex pb-5 ml-4">
+         <Image
+            src={restaurant.main_image}
+            alt={restaurant.name}
+            className="w-44 rounded object-cover"
+            width={0}
+            height={0}
+            sizes="100vw"
          />
          <div className="pl-5">
-            <h2 className="text-3xl">Aiāna Restaurant Collective</h2>
+            <h2 className="text-3xl">{restaurant.name}</h2>
             <div className="flex items-start">
                <div className="flex mb-2">*****</div>
                <p className="ml-2 text-sm">Awesome</p>
             </div>
             <div className="mb-9">
                <div className="font-light flex text-reg">
-                  <p className="mr-4">$$$</p>
-                  <p className="mr-4">Mexican</p>
-                  <p className="mr-4">Ottawa</p>
+                  <p className="mr-4">
+                     <Price price={restaurant.price} />
+                  </p>
+                  <p className="mr-4 capitalize">{restaurant.cuisine.name}</p>
+                  <p className="mr-4 capitalize">{restaurant.location.name}</p>
                </div>
             </div>
             <div className="text-red-600">
-               <Link href="/restaurant/milestones-stones">
+               <Link href={`/restaurant/${restaurant.slug}`}>
                   View more information
                </Link>
             </div>
