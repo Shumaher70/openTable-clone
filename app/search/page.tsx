3b979d7f2099644fe7d64b966545958db956fb29
oleg <olegvraby@gmail.com>
2023-10-26 18:21:10 +0200

    add dynamic data for RestaurantCart to the Search page

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,9 +1,38 @@
+import { PrismaClient } from '@prisma/client';
 import Header from './components/Header';
 import RestaurantCart from './components/RestaurantCart';
 import SearchSideBar from './components/SearchSideBar';
 import Head from './head';
 
-const Search = () => {
+const prisma = new PrismaClient();
+
+const fetchRestaurantByCity = (city: string) => {
+   const select = {
+      id: true,
+      name: true,
+      main_image: true,
+      cuisine: true,
+      slug: true,
+      location: true,
+      price: true,
+   };
+
+   if (!city) return prisma.restaurant.findMany({ select });
+
+   return prisma.restaurant.findMany({
+      where: {
+         location: {
+            name: city.toLowerCase(),
+         },
+      },
+      select,
+   });
+};
+
+const Search = async ({ searchParams }: { searchParams: { city: string } }) => {
+   const restaurants = await fetchRestaurantByCity(searchParams.city);
+   const noRestaurants = <p>Sorry we found no restaurants in this area</p>;
+
    return (
       <>
          <Head />
@@ -11,7 +40,11 @@ const Search = () => {
          <div className="flex py-4 m-auto w-2/3 justify-between items-start">
             <SearchSideBar />
             <div className="w-5/6">
-               <RestaurantCart />
+               {restaurants.length
+                  ? restaurants.map((item) => (
+                       <RestaurantCart key={item.slug} restaurant={item} />
+                    ))
+                  : noRestaurants}
             </div>
          </div>
       </>
