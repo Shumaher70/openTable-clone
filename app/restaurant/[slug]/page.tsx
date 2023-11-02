4b17f6c42794c4731f10441dc602a189e0c53b2f
oleg <olegvraby@gmail.com>
2023-11-02 18:12:04 +0200

    add:RestaurantDetails: add reviews to the selection

--- app/restaurant/[slug]/page.tsx
+++ app/restaurant/[slug]/page.tsx
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client';
+import { PrismaClient, Review } from '@prisma/client';
 import Description from './components/Description';
 import Images from './components/Images';
 import Rating from './components/Rating';
@@ -16,6 +16,7 @@ interface RestaurantDetailsType {
    images: string[];
    description: string;
    slug: string;
+   reviews: Review[];
 }
 
 const fetchRestaurantBySlug = async (
@@ -31,6 +32,7 @@ const fetchRestaurantBySlug = async (
          images: true,
          description: true,
          slug: true,
+         reviews: true,
       },
    });
 
@@ -50,10 +52,10 @@ const RestaurantDetails = async ({ params }: { params: { slug: string } }) => {
          <div className="bg-white w-[70%] rounded p-3 shadow">
             <RestaurantNavBar slug={restaurant.slug} />
             <Title name={restaurant.name} />
-            <Rating />
+            <Rating reviews={restaurant.reviews} />
             <Description description={restaurant.description} />
             <Images images={restaurant.images} />
-            <Reviews />
+            <Reviews reviews={restaurant.reviews} />
          </div>
          <div className="w-[27%] relative text-reg">
             <ReservationCart />
