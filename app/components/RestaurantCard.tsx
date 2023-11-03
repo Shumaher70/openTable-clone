46352e813b162aa9172b09a0d784c6b7e43186e5
oleg <olegvraby@gmail.com>
2023-11-03 17:23:55 +0200

    add:restaurantCard: add Stars component

--- app/components/RestaurantCard.tsx
+++ app/components/RestaurantCard.tsx
@@ -2,6 +2,7 @@ import Link from 'next/link';
 import { RestaurantCardType } from '../page';
 import Image from 'next/image';
 import Price from './Price';
+import Stars from './Stars';
 
 interface RestaurantCardProps {
    restaurant: RestaurantCardType;
@@ -22,7 +23,7 @@ const RestaurantCard = ({ restaurant }: RestaurantCardProps) => {
             <div className="p-1">
                <h3 className="font-bold text-2xl mb-2">{restaurant.name}</h3>
                <div className="flex items-start">
-                  <div className="flex mb-2">*****</div>
+                  <Stars reviews={restaurant.reviews} />
                   <p className="ml-2">
                      {restaurant.reviews.length} review
                      {restaurant.reviews.length > 1 ? 's' : ''}
