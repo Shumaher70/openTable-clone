3061a2f18e16196944386dc7b281db2e4191039a
oleg <olegvraby@gmail.com>
2023-11-06 17:55:09 +0200

    add:restaurant/not-found

---
+++ app/restaurant/not-found.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import Image from 'next/image';
+import errorMascot from '../../public/icons/error.png';
+
+const error = ({ error }: { error: Error }) => {
+   return (
+      <div className="h-screen bg-gray-200 flex flex-col justify-center items-center">
+         <Image src={errorMascot} alt="error" className="w-56 mb-8" />
+         <div className="bg-white p-9 py-14 shadow rounded">
+            <h3 className="text-3xl font-bold">Well, this is embarrassing</h3>
+            <p className="text-reg font-bold">
+               We could not find that restaurant{' '}
+            </p>
+            <p className="mt-6 text-sm font-light">Error Code: 400</p>
+         </div>
+      </div>
+   );
+};
+
+export default error;
